d5a17a9448ac1a52eeb805f80b2a4f8fdfe1dd83
Trever Anderson <treveranderson01@gmail.com>
2020-06-19 16:07:00 -0500

    output cv models

--- models/neural_net.py
+++ models/neural_net.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import ParameterGrid
+from tqdm import tqdm
 
 def standardize(df):
     for f in list(df)[:-1]:
@@ -33,11 +34,12 @@ def complex_model(kernel_regularizer):
 def evaluate(model_params, xs, ys, tr_params, bm_params):
     st_kf = StratifiedKFold(n_splits=5)
     row_keys = ['train_loss', 'train_accuracy', 'test_loss', 'test_accuracy', 
-                'train_params', 'model_params', 'model_name']
+                'train_params', 'model_params', 'model_name', 'test_ys', 'preds']
     cv_results = {k: [] for k in row_keys}
     ys_ps = {mod_func.__name__: [] for mod_func, _ in model_params}
     for mod_func, params in model_params:
-        for ps in list(ParameterGrid(params)):
+        print("Training {}".format(mod_func.__name__))
+        for ps in tqdm(list(ParameterGrid(params))):
             for tr_ind, tst_ind in st_kf.split(xs, ys):
                 tr_x, tr_y = xs[tr_ind], ys[tr_ind]
                 tst_x, tst_y = xs[tst_ind], ys[tst_ind]
@@ -47,33 +49,44 @@ def evaluate(model_params, xs, ys, tr_params, bm_params):
                 th = train_model.fit(tr_x, tr_y, **tps)
                 tr_l, tr_a = th.history['loss'][-1], th.history['accuracy'][-1]
                 tst_l, tst_a = train_model.evaluate(tst_x, tst_y, verbose=0)
-
+                
                 cv_results[row_keys[0]].append(tr_l)
                 cv_results[row_keys[1]].append(tr_a)
                 cv_results[row_keys[2]].append(tst_l)
                 cv_results[row_keys[3]].append(tst_a)
-                cv_results[row_keys[4]].append(tps)
-                cv_results[row_keys[5]].append(bmps)
+                cv_results[row_keys[4]].append(tuple(tps.items()))
+                cv_results[row_keys[5]].append(tuple(bmps.items()))
                 cv_results[row_keys[6]].append(mod_func.__name__)
+                cv_results[row_keys[7]].append(tst_y)
+                cv_results[row_keys[8]].append(train_model.predict(tst_x))
 
-                ys_ps[mod_func.__name__].append((tst_y, train_model.predict(tst_x)))
+    return pd.DataFrame(cv_results)
 
-    return (pd.DataFrame(cv_results), ys_ps)
+def cv_results(cv_data):
+    agg_funcs = ['mean', 'std', 'min', 'max']
+    output = cv_data.groupby(['model_name', 'model_params', 'train_params'])\
+                   .agg({'test_accuracy': agg_funcs, 
+                         'train_accuracy': agg_funcs, 
+                         'test_loss': agg_funcs, 
+                         'train_loss': agg_funcs})[['train_loss', 'train_accuracy', 'test_loss', 'test_accuracy']]
+    return output
+    
 
 def main(data_file, plot_dir):
     data = funcs.read_data(data_file)
     standardize(data)
     xs, ys = funcs.xs_and_ys(data)
 
-    model_params = [(simple_model, {'epochs': [1], 'batch_size': [5], 
+    model_params = [(simple_model, {'epochs': [5, 10], 'batch_size': [3, 4], 
                                     'verbose': [0], 'kernel_regularizer': ['l1', 'l2']}),
-                    (complex_model, {'epochs': [1], 'batch_size': [5], 
+                    (complex_model, {'epochs': [10, 15], 'batch_size': [3, 4], 
                                      'verbose': [0], 'kernel_regularizer': ['l1', 'l2']})]
+
     train_params = {'epochs', 'verbose', 'batch_size'}
     build_model_params = {'kernel_regularizer'}
-    cv_data, ys_ps = evaluate(model_params, xs, ys, train_params, build_model_params)
-
-    print("Cross validation results:\n", cv_data)
-
-    for mn, ps in ys_ps.items():
-        funcs.roc_curve(ps, plot_dir, mn)
+    cv_data = evaluate(model_params, xs, ys, train_params, build_model_params)
+    out_data = cv_data.sort_values(by=['test_loss', 'test_accuracy'])[['train_loss', 'train_accuracy', 'test_loss', 'test_accuracy', 'model_name']]
+    
+    res = cv_results(cv_data)
+    print("CV accuracy results: ", res[[('test_accuracy', 'mean'), ('test_accuracy', 'std')]]\
+          .sort_values(by=[('test_accuracy', 'mean')], ascending=False))
